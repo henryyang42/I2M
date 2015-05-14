cadfcec4c9978b14c9ab4f6f7905cbb5ddcc6e05
henryyang42 <henryyang42@gmail.com>
2015-05-14 22:54:16 +0800

    finish shit

--- HW3/hw3_2.m
+++ HW3/hw3_2.m
@@ -2,24 +2,30 @@ clear all;close all;clc;
 
 [y, Fs] = audioread('AnJing_4bit.wav');
 [n, ch] = size(y);
+y = y * 128;
 %% Plot the spectrum and shape
-subplot(1,2,1);
+subplot(2,4,1);
 plot_spectrum(y, Fs, 'Original spectrum');
-subplot(1,2,2);
-plot(y(:,1)); % Only plot one of its channel
+subplot(2,4,2);
+plot(y(50000:60000,1)); % Only plot part of one of its channel
 title('Original shape');
 
 %% Add random noise
-F_in = y ;
+F_in = y;
 for i = 1:n
 	for j = 1:ch
-		F_in(i, j) = F_in(i, j) +  (rand - 0.5);
+		F_in(i, j) = F_in(i, j) +  (rand - 0.5)*16;
 	end
 end
+subplot(2,4,3);
+plot_spectrum(F_in, Fs, 'Noised spectrum');
+subplot(2,4,4);
+plot(F_in(50000:60000,1)); % Only plot part of one of its channel
+title('Noised shape');
 
 %% First-order feedback loop for noise shaping
-F_out = y;
-c = 0.8;
+F_out = F_in;
+c = 0.9;
 for i = 1:n
 	for j = 1:ch
 		if i == 1
@@ -30,9 +36,18 @@ for i = 1:n
 		F_out(i, j) = F_in(i, j) + c * Ei;
 	end
 end
+subplot(2,4,5);
+plot_spectrum(F_out, Fs, 'Shapped spectrum');
+subplot(2,4,6);
+plot(F_out(50000:60000,1)); % Only plot part of one of its channel
+title('Shapped shape');
 
 [F_out(:,1), ~] = myFilter(F_out(:,1) , Fs, 1001, 'Hamming', 'low-pass', 500);
 [F_out(:,2), ~] = myFilter(F_out(:,2) , Fs, 1001, 'Hamming', 'low-pass', 500);
+subplot(2,4,7);
+plot_spectrum(F_out, Fs, 'Low-passed spectrum');
+subplot(2,4,8);
+plot(F_out(50000:60000,1)); % Only plot part of one of its channel
+title('Low-passed shape');
 
-size(F_out)
-audiowrite('qaq.wav', F_out, Fs);
+audiowrite('AnJing_4bit_shaped.wav', F_out/128, Fs);
---
+++ HW3/readme.txt
@@ -0,0 +1,16 @@
+Introduction to Multimedia Homework 3
+
+Part 1:
+    Eexecute `hw3_1.m` matlab script.
+	Input
+		- hw3_mix.wav	
+	Output
+		- low_pass.wav
+		- band_pass.wav
+		- high_pass.wav
+Part 2:
+    Eexecute `hw3_2.m` matlab script.
+	Input
+		- AnJing_4bit.wav		
+	Output
+		- AnJing_4bit_shaped.wav	
